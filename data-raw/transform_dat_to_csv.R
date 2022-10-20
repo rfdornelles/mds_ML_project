4d1d77e729355512f33e8f3ac545a51b2bc473c8
Rodrigo Dornelles <31113760+rfdornelles@users.noreply.github.com>
2022-10-20 10:38:05 +0200

    transforming .dat files into .csv using .R script

---
+++ data-raw/transform_dat_to_csv.R
@@ -0,0 +1,22 @@
+df1 <- read.table("data-raw/RCP85_MIDYR_CONC.DAT", skip = 40)
+
+`colnames`(df1) <- c("YEARS", "CO2EQ", "KYOTO-CO2EQ", "CO2", "CH4", "N2O", "FGASSUMHFC134AEQ",
+                    "MHALOSUMCFC12EQ", "CF4","C2F6","C6F14", "HFC23", "HFC32", "HFC43_10",
+                    "HFC125", "HFC134a", "HFC143a", "HFC227ea", "HFC245fa", "SF6", "CFC_11",
+                    "CFC_12", "CFC_113", "CFC_114", "CFC_115","CARB_TET", "MCF",
+                    "HCFC_22", "HCFC_141B", "HCFC_142B", "HALON1211", "HALON1202", "HALON1301",
+                    "HALON2402", "CH3BR", "CH3CL")
+
+readr::write_csv(df1, "data-raw/RCP85_MIDYR_CONC.csv")
+
+
+df2 <- read.table("data-raw/RCP3PD_MIDYR_CONC.DAT", skip = 40) 
+
+`colnames`(df2) <- c("YEARS", "CO2EQ", "KYOTO-CO2EQ", "CO2", "CH4", "N2O", "FGASSUMHFC134AEQ",
+                     "MHALOSUMCFC12EQ", "CF4","C2F6","C6F14", "HFC23", "HFC32", "HFC43_10",
+                     "HFC125", "HFC134a", "HFC143a", "HFC227ea", "HFC245fa", "SF6", "CFC_11",
+                     "CFC_12", "CFC_113", "CFC_114", "CFC_115","CARB_TET", "MCF",
+                     "HCFC_22", "HCFC_141B", "HCFC_142B", "HALON1211", "HALON1202", "HALON1301",
+                     "HALON2402", "CH3BR", "CH3CL")
+
+readr::write_csv(df2, "data-raw/RCP3PD_MIDYR_CONC.csv")
