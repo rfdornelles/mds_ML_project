93947469e46ad7049a329bbff47d51837cdc3363
Rodrigo Dornelles <31113760+rfdornelles@users.noreply.github.com>
2022-11-09 19:37:11 +0100

    generating the 2nd dataset clean

---
+++ R/merge_datasets_v2.R
@@ -0,0 +1,97 @@
+# testing the merged datasets
+
+
+# Import all data ---------------------------------------------------------
+
+
+# temperature
+# temp_df <- readr::read_csv("data-raw/nasa_temperature.csv") |> 
+#   janitor::clean_names() |> 
+#   dplyr::select(year, glob, n_hem, x24n_90n) |> 
+#   dplyr::mutate(year = as.numeric(year))
+
+# german pop
+
+wb_pop <- readr::read_csv("data-raw/WBI_population_API_SP.POP.TOTL_DS2_en_csv_v2_4701113.csv",
+                          skip = 4) 
+
+nwb_pop <- wb_pop |> 
+#  dplyr::filter(`Country Name` == "Germany") |> 
+  dplyr::select(-2:-4, -`...67`) |> 
+  dplyr::rename(country = `Country Name`) |> 
+  tidyr::pivot_longer(
+    col = where(is.numeric), 
+    values_to = "population",
+    names_to = "year") |> 
+  dplyr::mutate(year = as.numeric(year))
+
+# deaths by heat/cold
+
+#TODO decide if we use all columns
+#TODO decide if Germany only should be used, or all countries, or all except
+db_annual_deaths <- readr::read_csv("data-raw/annual-number-of-deaths-by-cause.csv")
+
+db_annual_deaths <- db_annual_deaths |> 
+  dplyr::select("Entity", "Code", "Year",
+                "Deaths - Environmental heat and cold exposure - Sex: Both - Age: All Ages (Number)",
+                "Deaths - Lower respiratory infections - Sex: Both - Age: All Ages (Number)",
+                "Deaths - Exposure to forces of nature - Sex: Both - Age: All Ages (Number)",
+                "Deaths - Chronic respiratory diseases - Sex: Both - Age: All Ages (Number)"
+  ) |> 
+  janitor::clean_names() |> 
+  dplyr::rename(
+    "qnt_death_heat_cold_exposure" = 4,
+    "qnt_death_lower_respiratory_infections" = 5,
+    "qnt_death_exposure_to_forces_of_nature" = 6,
+    "qnt_death_chronic_respiratory_diseases" = 7
+  )
+
+# db_annual_deaths <- db_annual_deaths |> 
+#  # dplyr::select(-1:-2) |> 
+#   dplyr::group_by(year) |> 
+#   dplyr::summarise(
+#     dplyr::across(.fns = sum, na.rm = TRUE)
+#   )  |> 
+#   dplyr::mutate(year = as.numeric(year))
+
+# deaths germany
+deaths_germany <- readr::read_csv("data/german_deaths.csv") |> 
+  janitor::clean_names()  |> 
+  dplyr::mutate(year = as.numeric(year))
+
+
+# projected pop (predict?)
+proj_pop_germany <- readr::read_csv(
+  "data/Projected German Population 2050.csv")  |> 
+  dplyr::mutate(year = as.numeric(year))
+
+#  temperature rcp 45 -----------------------------------------------------
+
+temp_rcf_45 <- readr::read_csv("data/temp_rcp_45.csv")
+
+# emissions 
+
+emissions_rcp85 <- readr::read_csv("data/RCP85_MIDYR_CONC.csv") |> 
+  dplyr::rename(year = YEARS)
+
+# merge all ---------------------------------------------------------------
+
+karon_dataset <- wb_pop |> 
+  dplyr::left_join(temp_df) |> 
+  dplyr::left_join(db_annual_deaths) |> 
+ # dplyr::left_join(deaths_germany) |> 
+  dplyr::left_join(temp_rcf_45) |> 
+  dplyr::left_join(emissions_rcp85)
+
+readr::write_csv(karon_dataset, "data/karon_dataset.csv")
+
+
+# clean years: 1990 to 2021 -----------------------------------------------
+
+karon_clean <- karon_dataset |> 
+  dplyr::filter(year >= 1990 & year <= 2019)
+
+# karon_clean |> tibble::view()
+
+readr::write_csv(karon_clean, "data/karon_clean.csv")
+
---
+++ R/wrangling_all_countries.R
@@ -0,0 +1,166 @@
+# boundaries 
+# https://data.humdata.org/dataset/bounding-boxes-for-countries
+boundaries <- readr::read_csv("data-raw/country-boundingboxes.csv") |> 
+  dplyr::select(1, 3:6)
+
+boundaries
+
+###
+
+ncfile <- "data-raw/mean_Tmean_Yearly_rcp45_mean_v1.0.nc"
+
+rcp45 <- tidync::tidync(ncfile)
+
+
+time_nc <- rcp45 |>  
+  tidync::activate("D2") |> 
+  tidync::hyper_array()
+
+tunit <- ncmeta::nc_atts(ncfile, "time") |> 
+  tidyr::unnest(cols = c(value)) |> 
+  dplyr::filter(name == "units")
+
+time_parts <- RNetCDF::utcal.nc(tunit$value, time_nc$time)
+
+# ## convert to date-time
+# ISOdatetime(time_parts[,"year"], 
+#             time_parts[,"month"], 
+#             time_parts[,"day"], 
+#             time_parts[,"hour"], 
+#             time_parts[,"minute"], 
+#             time_parts[,"second"])
+
+years_ref <- tibble::tibble(
+  time = time_nc$time,
+  year = time_parts[,"year"]
+)
+
+rcp45_tibble <- rcp45 |> 
+  tidync::activate("mean_Tmean_Yearly") |> 
+  tidync::hyper_tibble() 
+
+# getting the boundaries
+json_url <- "https://gist.githubusercontent.com/cplpearce/3bc5f1e9b1187df51d2085ffca795bee/raw/b36904c0c8ea72fdb82f68eb33f29891095deab3/country_codes"
+
+json <- jsonlite::fromJSON(json_url, flatten = TRUE)
+
+json_boundaries <-names(json) |> 
+  purrr::map_df(.f = ~{
+    
+    json |> 
+      purrr::pluck(.x) |> 
+      purrr::pluck("boundingBox") |> 
+      purrr::flatten_dfc() |> 
+      dplyr::mutate(
+        country = .x
+      )
+  }
+  )
+
+codes <- names(json) |> 
+  purrr::map_df(.f = ~{
+  
+    json |> 
+      purrr::pluck(.x) |> 
+      purrr::pluck("name") |> 
+      tibble::as_tibble() |> 
+    #  purrr::flatten_dfc() |> 
+      dplyr::mutate(
+        code = .x
+      )
+  }
+  )
+
+boundaries <- json_boundaries |> 
+  dplyr::rename(
+    latmin = `lat...1`,
+    longmin = `lon...2`,
+    latmax = `lat...3`,
+    longmax = `lon...4`
+  ) |> 
+  dplyr::mutate(
+    dplyr::across(
+      where(is.numeric),
+      round, 1
+  )
+  )
+
+temp_countries <- purrr::map_df(
+  .x = 1:nrow(boundaries),
+  .f = ~{
+  i = .x
+  
+  rcp45_tibble |> 
+    dplyr::filter(
+      lon >= boundaries$longmin[i] & lon <= boundaries$longmax[i] &
+        lat >= boundaries$latmin[i] & lon <= boundaries$latmin[i]
+    ) |> 
+    dplyr::group_by(time) |> 
+    dplyr::summarise(
+      temp = mean(mean_Tmean_Yearly, na.rm = T)
+    ) |> 
+    dplyr::mutate(
+      country = boundaries$country[i]
+    )
+})
+
+# beepr::beep()
+
+## merge names
+temp_countries <- temp_countries |> 
+  dplyr::left_join(codes, by = c("country" = "code"))
+
+## merge years
+temp_countries <- temp_countries |> 
+  dplyr::left_join(years_ref) |> 
+  dplyr::select(
+    year, country = value, temp
+  ) 
+
+temp_countries <- temp_countries |> 
+  dplyr::filter(year <= 2019)
+
+## merge population
+wb_pop <- readr::read_csv("data-raw/WBI_population_API_SP.POP.TOTL_DS2_en_csv_v2_4701113.csv",
+                          skip = 4) 
+
+wb_pop <- wb_pop |> 
+  #  dplyr::filter(`Country Name` == "Germany") |> 
+  dplyr::select(-2:-4, -`...67`) |> 
+  dplyr::rename(country = `Country Name`) |> 
+  tidyr::pivot_longer(
+    col = where(is.numeric), 
+    values_to = "population",
+    names_to = "year") |> 
+  dplyr::mutate(year = as.numeric(year))
+
+## deaths
+
+db_annual_deaths <- readr::read_csv("data-raw/annual-number-of-deaths-by-cause.csv")
+
+db_annual_deaths <- db_annual_deaths |> 
+  dplyr::select("country" = "Entity", "Code", "Year",
+                "Deaths - Environmental heat and cold exposure - Sex: Both - Age: All Ages (Number)",
+                "Deaths - Lower respiratory infections - Sex: Both - Age: All Ages (Number)",
+                "Deaths - Exposure to forces of nature - Sex: Both - Age: All Ages (Number)",
+                "Deaths - Chronic respiratory diseases - Sex: Both - Age: All Ages (Number)"
+  ) |> 
+  janitor::clean_names() |> 
+  dplyr::rename(
+    "qnt_death_heat_cold_exposure" = 4,
+    "qnt_death_lower_respiratory_infections" = 5,
+    "qnt_death_exposure_to_forces_of_nature" = 6,
+    "qnt_death_chronic_respiratory_diseases" = 7
+  )
+
+temp_countries |> 
+  dplyr::left_join(wb_pop) |> 
+  dplyr::left_join(db_annual_deaths) |> 
+  dplyr::select(year, 
+                country, temp, population, qnt_death_heat_cold_exposure)
+
+# |> 
+#   tibble::view() |> 
+#   dplyr::filter(is.na(population) | is.na(temp) | is.na(qnt_death_heat_cold_exposure))
+  
+
